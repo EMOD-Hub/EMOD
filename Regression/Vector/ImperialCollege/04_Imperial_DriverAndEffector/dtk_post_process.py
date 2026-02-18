7089306e9ed2dc55fc7f13966f7797134a1457c2
Svetlana Titova <svetlana.titova@gatesfoundation.org>
2026-02-17 16:23:24 -0800

    updating output files to be linux-generated to they pas on linux part 8 (#177)

--- Regression/Vector/ImperialCollege/04_Imperial_DriverAndEffector/dtk_post_process.py
+++ Regression/Vector/ImperialCollege/04_Imperial_DriverAndEffector/dtk_post_process.py
@@ -17,71 +17,75 @@ def CreateHeader( start_time, num_timesteps, timestep, num_channels ):
 
         
 def application( output_path ):
+    files_to_convert = []
+    for fn in os.listdir( output_path ):
+        if fn.startswith("ReportVectorGenetics") and fn.endswith( ".csv" ):
+            files_to_convert.append(os.path.join( output_path, fn ) )
 
-    csv_fn = os.path.join( output_path, "ReportVectorGenetics_arabiensis_Female_GENOME.csv" )
-    json_fn = csv_fn.replace( "csv", "json" )
+    for csv_fn in files_to_convert:
+        json_fn = csv_fn.replace( "csv", "json" )
 
-    # data for header
-    start_time = 0
-    num_timesteps = 0
-    timestep = 1 # assume vectors so assume 1
-    num_channels = 0
-    
-    json_data = {}
-    json_data[ "Channels" ] = {}
-    
-    with open( csv_fn, "r" ) as rs:
-        # column names should be Time, NodeID, Genome, VectorPopulation
-        header = rs.readline()
-        header = header.rstrip()
-        column_names = header.split(",")
-        if( len(column_names) != 4 ):
-            print("Expected 4 columns got "+len(column_names))
-            exit(-1)
-        
-        channel_names = []
-        found_channels = False
-        first_values = []
-        while( not found_channels ):
-            line = rs.readline()
-            line = line.rstrip()
-            values = line.split(",")
-            if( values[0] != '0' ):
-                found_channels = True
-            else:
-                channel_names.append( values[2] )
-            first_values.append( values )
-            
-        num_channels = len(channel_names)
-        for name in channel_names:
-            json_data[ "Channels" ][ name ] = {}
-            json_data[ "Channels" ][ name ][ "Units" ] = ""
-            json_data[ "Channels" ][ name ][ "Data"  ] = []
+        # data for header
+        start_time = 0
+        num_timesteps = 0
+        timestep = 1 # assume vectors so assume 1
+        num_channels = 0
 
+        json_data = {}
+        json_data[ "Channels" ] = {}
 
-        for values in first_values:
-            name = values[2]
-            json_data[ "Channels" ][ name ][ "Data" ].append( float(values[ 3 ]) )
-        
+        with open( csv_fn, "r" ) as rs:
+            # column names should be Time, NodeID, Genome, VectorPopulation
+            header = rs.readline()
+            header = header.rstrip()
+            column_names = header.split(",")
+            if( len(column_names) != 4 ):
+                print("Expected 4 columns got "+len(column_names))
+                exit(-1)
 
-        for line in rs:
-            line = line.rstrip()
-            values = line.split(",")
-            name = values[2]
-            json_data[ "Channels" ][ name ][ "Data" ].append( float(values[ 3 ]) )
+            channel_names = []
+            found_channels = False
+            first_values = []
+            while( not found_channels ):
+                line = rs.readline()
+                line = line.rstrip()
+                values = line.split(",")
+                if( values[0] != '0' ):
+                    found_channels = True
+                else:
+                    channel_names.append( values[2] )
+                first_values.append( values )
+
+            num_channels = len(channel_names)
+            for name in channel_names:
+                json_data[ "Channels" ][ name ] = {}
+                json_data[ "Channels" ][ name ][ "Units" ] = ""
+                json_data[ "Channels" ][ name ][ "Data"  ] = []
 
 
-    num_timesteps = len( json_data[ "Channels" ][ channel_names[0] ][ "Data" ] )
+            for values in first_values:
+                name = values[2]
+                json_data[ "Channels" ][ name ][ "Data" ].append( float(values[ 3 ]) )
 
-    inset_json = CreateHeader( start_time, num_timesteps, timestep, num_channels )
-    
-    json_data[ "Header" ] = inset_json
-    
-    with open( json_fn, "w" ) as handle:
-        handle.write( json.dumps( json_data, indent=4, sort_keys=False ) )
-    
-    
-    print("Done converting ReportVectorStats")
+
+            for line in rs:
+                line = line.rstrip()
+                values = line.split(",")
+                name = values[2]
+                json_data[ "Channels" ][ name ][ "Data" ].append( float(values[ 3 ]) )
+
+
+        num_timesteps = len( json_data[ "Channels" ][ channel_names[0] ][ "Data" ] )
+
+        inset_json = CreateHeader( start_time, num_timesteps, timestep, num_channels )
+
+        json_data[ "Header" ] = inset_json
+
+        with open( json_fn, "w" ) as handle:
+            handle.write( json.dumps( json_data, indent=4, sort_keys=False ) )
+
+        os.remove(csv_fn)
+        print("Done converting ReportVectorStats")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
